c390c4c42d978a4c25529162914f336258986641
Daniela Gluhova <stu2401717007@uni-plovdiv.bg>
2026-06-03 21:34:38 +0300

    move database credentials to environment variables and hide them with gitignore, issue #3

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 import app from "./app.js";
 import { connectDb } from "./connectdb.js";
 import "./models/index.js";
+import "dotenv/config";
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 async function startServer() {
   try {
     await connectDb();
 
-    app.listen(PORT,"0.0.0.0", () => {
+    app.listen(PORT, () => {
       console.log(`🚀 Server running on http://localhost:${PORT}`);
     });
   } catch (error) {
